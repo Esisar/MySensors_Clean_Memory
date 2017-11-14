8248b605792bbe479c627305bc8f9cbaba0e5a44
Darksalamander85 <laurent.guilloton.dev@gmail.com>
2017-11-14 15:29:15 +0100

    Initial commit

---
+++ .ino.cpp
@@ -0,0 +1,17 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-09-27 21:46:46
+
+#include "Arduino.h"
+#define MY_CORE_ONLY
+#include "Arduino.h"
+#include <MySensors.h>
+void setup() ;
+void loop() ;
+
+#include "MySensors_Clean_Memory.ino"
+
+
+#endif
---
+++ README.md
@@ -0,0 +1,1 @@
+# MySensors_Clean_Memory
